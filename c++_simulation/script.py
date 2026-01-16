96b75f60fada2720c1b64fc16b8259464a57e7fc
jakubstach <jakub.stach1102@gmail.com>
2026-01-16 12:54:59 +0100

    live plotting first draft

---
+++ c++_simulation/script.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+import pandas as pd
+import subprocess
+import time
+import csv
+path = "simulation.exe"
+subprocess.Popen([path])
+
+#style.use('fivethirtyeight')
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+time.sleep(0.2)
+
+def plotting(i):
+    time = []
+    grass = []
+    rabbits = []
+    with open("data/data.csv") as f:
+        table = csv.reader(f)
+        for row in table:
+            try:
+                time.append(float(row[0]))
+                grass.append(int(row[1]))
+                rabbits.append(int(row[2]))
+            except:
+                print("")
+        time.pop(0)
+        grass.pop(0)
+        rabbits.pop(0)
+    ax.clear()
+    ax.plot(time, grass, color="green")
+    ax.plot(time, rabbits, color="grey")
+
+animation_object = animation.FuncAnimation(fig, plotting, interval=1000)
+plt.show()
+
+
